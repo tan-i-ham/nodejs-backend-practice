8cd97555a0f0d0ce043049423e501c539bae31ca
tp6hannah <hannah51125@gmail.com>
2020-06-24 07:40:49 +0900

    use mongoose to connect to db

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
 // load env variable
 dotenv.config({ path: './config/config.env' });
+
+// connect to db
+connectDB();
 
 const app = express();
 
 // const logger = require('./middlewares/logger');
 // app.use(logger);
 if ((process.env.NODE_ENV = 'development')) {
-  app.use(morgan('dev'));
+    app.use(morgan('dev'));
 }
 
 const PORT = process.env.PORT || 5000;
@@ -19,14 +23,21 @@ const bootcamp = require('./routes/bootcamps');
 // Mount router
 app.use('/api/v1/bootcamps', bootcamp);
 
-// route
-app.get('/', (req, res) => {
-  // res.send("hello from express app!!")
-  //res.send({ name: "hannah" }) // res.json({ name: "hannah" })
-  // res.sendStatus(404);
-  res.status(404).json({ msg: 'error' });
+// basic route
+// app.get('/', (req, res) => {
+//     res.send('hello from express app!!');
+//     res.send({ name: 'hannah' }); // res.json({ name: "hannah" })
+//     res.sendStatus(404);
+//     res.status(404).json({ msg: 'error' });
+// });
+
+const server = app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT} in ${process.env.NODE_ENV}!`);
 });
 
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT} in ${process.env.NODE_ENV}!`);
+// Handle unhandled promise rejections
+process.on('unhadledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    // Close server
+    server.close(() => process.exit(1));
 });
